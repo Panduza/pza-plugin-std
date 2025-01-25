23a4820c7169cbd4fbe34f1305654df616a3b9a2
XdoctorwhoZ <xavier.rodriguezalvarez@gmail.com>
2025-01-25 07:19:44 +0100

    up scpi

--- src/scpi/device.rs
+++ src/scpi/device.rs
@@ -2,9 +2,7 @@ use async_trait::async_trait;
 use panduza_platform_core::connector::usb::tmc::Driver as UsbTmcDriver;
 use panduza_platform_core::connector::usb::Settings as UsbSettings;
 use panduza_platform_core::{log_debug, DriverOperations, Error, Instance};
-use std::sync::Arc;
 use std::time::Duration;
-use tokio::sync::Mutex;
 use tokio::time::sleep;
 
 // pub mod data;
@@ -17,62 +15,23 @@ use tokio::time::sleep;
 ///
 pub struct Device {}
 
-impl Device {
-    // ///
-    // /// Prepare settings of the device
-    // ///
-    // pub async fn prepare_settings(&mut self, device: Device) -> Result<(), Error> {
-    //     // Get the device logger
-    //     let logger = device.logger.clone();
-
-    //     // Get the device settings
-    //     let json_settings = device
-    //         .settings()
-    //         .await
-    //         .or(Some(serde_json::Value::Null))
-    //         .unwrap();
-
-    //     // Log debug info
-    //     logger.info("Build interfaces for \"picoha.dio\" device");
-    //     logger.info(format!("JSON settings: {:?}", json_settings));
-
-    //     // // Usb settings
-    //     // let usb_settings = UsbSettings::new()
-    //     //     .set_vendor(PICOHA_VENDOR_ID)
-    //     //     .set_model(PICOHA_PRODUCT_ID)
-    //     //     .optional_set_serial_from_json_settings(&json_settings);
-    //     // logger.info(format!("USB settings: {:?}", usb_settings));
-
-    //     // Serial settings
-    //     self.serial_settings = Some(
-    //         SerialSettings::new()
-    //             .set_port_name_from_json_or_usb_settings(&json_settings, &usb_settings)
-    //             .map_err(|e| Error::Generic(e.to_string()))?
-    //             .set_baudrate(PICOHA_SERIAL_BAUDRATE)
-    //             .set_read_timeout(Duration::from_secs(2)),
-    //     );
-    //     logger.info(format!(
-    //         "SERIAL settings: {:?}",
-    //         self.serial_settings.as_ref().unwrap()
-    //     ));
-
-    //     Ok(())
-    // }
-}
+impl Device {}
 
 #[async_trait]
 impl DriverOperations for Device {
     ///
     /// Mount the device instance
     ///
-    async fn mount(&mut self, instance: Instance) -> Result<(), Error> {
+    async fn mount(&mut self, mut instance: Instance) -> Result<(), Error> {
         //
         //
         let logger = instance.logger.clone();
 
+        //
         // Usb settings
-
-        let settings = instance.settings().await.unwrap();
+        let settings = instance.settings().await.ok_or(Error::BadSettings(
+            "Usb Settings are required for this instance".to_string(),
+        ))?;
 
         //
         // Compose USB settings
@@ -91,9 +50,17 @@ impl DriverOperations for Device {
         // // endpoint_out: 0x01,
         // // max_packet_size: 512,
 
+        // let pok = instance
+        //     .create_attribute("pok")
+        //     .with_rw()
+        //     .finish_as_json()
+        //     .await?;
+
+        // pok.set(json!("Hello, World!")).await?;
+
         //
         // Mount the driver
-        let mut driver = UsbTmcDriver::open(&usb_settings, 0x81, 0x01, 512)?.into_arc_mutex();
+        let mut driver = UsbTmcDriver::open(&usb_settings)?.into_arc_mutex();
 
         panduza_platform_core::std::class::repl::mount("scpi", instance.clone(), driver).await?;
 
